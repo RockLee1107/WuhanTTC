ba05b9b711cab767bfd67d5e12539e5cba489eff
huangxiujie <huangxiujie@163.com>
2016-02-16 00:23:26 +0800

    Initial Commit

---
+++ Foundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foundation
+//
+//  Created by HuangXiuJie on 16/2/16.
+//  Copyright (c) 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Foundation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Foundation
+//
+//  Created by HuangXiuJie on 16/2/16.
+//  Copyright (c) 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
