5a270e52ec327db19ef89003da8078cf0865805d
黄秀杰 <huangxiujie@163.com>
2016-03-30 18:54:50 +0800

    登录布局美化

--- Foundation/Login/LoginViewController.m
+++ Foundation/Login/LoginViewController.m
@@ -7,56 +7,20 @@
 //
 
 #import "LoginViewController.h"
-#import "MainTabBarController.h"
-#import "User.h"
 #import "LocationUtil.h"
 
 @interface LoginViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
-@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
-
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 @end
 
 @implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    直接登录
-//    [self loginButtonPress:nil];
     [[LocationUtil getInstance] fetchLocation];
+    self.containerView.layer.cornerRadius = 6.0;
+    self.containerView.clipsToBounds = YES;
     // Do any additional setup after loading the view.
 }
-
-- (IBAction)loginButtonPress:(id)sender {
-    NSString *username = self.usernameTextField.text;
-    NSString *password = self.passwordTextField.text;
-    NSDictionary *param = @{@"username":username,
-                            @"password":password};
-    [self.service POST:@"login" parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(@"login:%@",responseObject);
-        User *user = [User getInstance];
-        user.username = responseObject[@"username"];
-        user.uid = responseObject[@"userId"];
-        MainTabBarController *vc = [[MainTabBarController alloc] init];
-        [[[UIApplication sharedApplication].windows firstObject] setRootViewController:vc];
-    } noResult:^{
-        
-    }];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
