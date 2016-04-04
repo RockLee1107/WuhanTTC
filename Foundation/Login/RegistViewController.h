2884b4e7c1a859132cd3dcbec341db8adcb2c11f
huangxiujie <huangxiujie@163.com>
2016-04-04 15:55:42 +0800

    注册

---
+++ Foundation/Login/RegistViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistViewController.h
+//  Foundation
+//
+//  Created by HuangXiuJie on 16/4/4.
+//  Copyright © 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface RegistViewController : BaseViewController
+
+@end
