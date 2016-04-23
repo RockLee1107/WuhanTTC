38928ad02659978e48eb70320fa073ab870f33e8
iMac <huangxiujie@163.com>
2016-04-23 16:57:12 +0800

    完成活动搜索

---
+++ Foundation/Activity/Search/ActivitySearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookSearchViewController.h
+//  Foundation
+//
+//  Created by Dotton on 16/4/22.
+//  Copyright © 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface ActivitySearchViewController : BaseTableViewController
+
+@end
