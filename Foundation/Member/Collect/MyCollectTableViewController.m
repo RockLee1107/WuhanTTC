a78a9f381957a7e0da23197b4f21044dff3062dd
iMac <huangxiujie@163.com>
2016-04-24 21:17:08 +0800

    按时间

--- Foundation/Member/Collect/MyCollectTableViewController.m
+++ Foundation/Member/Collect/MyCollectTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MyCollectTableViewController.h"
+#import "BookListDelegate.h"
 
 @interface MyCollectTableViewController ()
 
@@ -16,22 +17,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setDynamicLayout];
+    [self initDelegate];
+    [self initRefreshControl];
+    [self fetchData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//初始化代理
+- (void)initDelegate {
+    self.tableViewDelegate = [[BookListDelegate alloc] init];
+    self.tableViewDelegate.vc = self;
+    self.tableView.delegate = self.tableViewDelegate;
+    self.tableView.dataSource = self.tableViewDelegate;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//上拉下拉控件
+- (void)initRefreshControl {
+    /**上拉刷新、下拉加载*/
+    __weak typeof(self) weakSelf = self;
+    // 设置回调（一旦进入刷新状态就会调用这个refreshingBlock）
+    [self.tableView addLegendHeaderWithRefreshingBlock:^{
+        weakSelf.page.pageNo = 1;
+        [weakSelf fetchData];
+        [weakSelf.tableView.header endRefreshing];
+    }];
+    [self.tableView.legendHeader beginRefreshing];
+    [self.tableView addLegendFooterWithRefreshingBlock:^{
+        weakSelf.page.pageNo++;
+        [weakSelf fetchData];
+        // 拿到当前的上拉刷新控件，结束刷新状态
+        [weakSelf.tableView.footer endRefreshing];
+    }];
 }
-*/
 
+///请求网络
+- (void)fetchData {
+    NSDictionary *param =  @{@"QueryParams":[StringUtil dictToJson:@{
+                                                                     @"SEQ_collectUserId":[User getInstance].uid                                                                     }],
+                             @"Page":[StringUtil dictToJson:[self.page dictionary]]};
+    [self.service GET:@"book/searchSpBookList" parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [SVProgressHUD dismiss];
+        if (self.page.pageNo == 1) {
+            //由于下拉刷新时页面而归零
+            [self.tableViewDelegate.dataArray removeAllObjects];
+            [self.tableView.footer resetNoMoreData];
+        }
+        //当小于每页条数，就判定加载完毕
+        if ([responseObject[@"result"] count] < self.page.pageSize) {
+            [self.tableView.footer noticeNoMoreData];
+        }
+        [self.tableViewDelegate.dataArray addObjectsFromArray:responseObject[@"result"]];
+        [self.tableView reloadData];
+    } noResult:^{
+        if (self.page.pageNo == 1) {
+            [self.tableViewDelegate.dataArray removeAllObjects];
+            [self.tableView reloadData];
+        }
+        [self.tableView.footer noticeNoMoreData];
+    }];
+}
 @end
