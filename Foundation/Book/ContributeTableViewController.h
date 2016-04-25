01657c355ec111a0265fbd6b575e178d0935f402
huangxiujie <huangxiujie@163.com>
2016-04-25 11:55:59 +0800

    写推荐好文

---
+++ Foundation/Book/ContributeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContributeTableViewController.h
+//  Foundation
+//
+//  Created by HuangXiuJie on 16/4/25.
+//  Copyright © 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import "BaseStaticTableViewController.h"
+
+@interface ContributeTableViewController : BaseStaticTableViewController
+
+@end
---
+++ Foundation/Book/ContributeTableViewController.m
@@ -0,0 +1,50 @@
+//
+//  ContributeTableViewController.m
+//  Foundation
+//
+//  Created by HuangXiuJie on 16/4/25.
+//  Copyright © 2016年 瑞安市灵犀网络技术有限公司. All rights reserved.
+//
+
+#import "ContributeTableViewController.h"
+#import "EMTextView.h"
+#import "LXButton.h"
+#import "VerifyUtil.h"
+
+@interface ContributeTableViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField; //名称
+@property (weak, nonatomic) IBOutlet EMTextView *contentTextView;  //活动详情
+@end
+
+@implementation ContributeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //    隐藏键盘
+    self.titleTextField.delegate = self;
+    // Do any additional setup after loading the view.
+}
+
+///提交到网络
+- (IBAction)postButtonPress:(id)sender {
+    if (![VerifyUtil hasValue:self.titleTextField.text]) {
+        [SVProgressHUD showErrorWithStatus:@"请输入正确的文章链接"];
+        return;
+    }
+    if (![VerifyUtil isValidStringLengthRange:self.contentTextView.text between:3 and:200]) {
+        [SVProgressHUD showErrorWithStatus:@"请输入帖子内容(1-1000字)"];
+        return;
+    }
+    NSDictionary *param = @{
+                            @"PostSubject":[StringUtil dictToJson:@{
+                                                                    @"title":self.titleTextField.text,
+                                                                    @"content":self.contentTextView.text
+                                                                    }]
+                            };
+    [self.service POST:@"book/contribute" parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [SVProgressHUD showSuccessWithStatus:@"发布成功"];
+        [self.navigationController popViewControllerAnimated:YES];
+    } noResult:nil];
+}
+
+@end
