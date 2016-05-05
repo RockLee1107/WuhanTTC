e4a445212f2c021dba328c80882bd22ff79a40b7
iMac <huangxiujie@163.com>
2016-05-05 16:59:05 +0800

    产品图片读取

--- Foundation/Project/Detail/ProjectSummaryTableViewController.m
+++ Foundation/Project/Detail/ProjectSummaryTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ProjectSummaryTableViewController.h"
 #import "SingletonObject.h"
+#import "LXGallery.h"
 
 @interface ProjectSummaryTableViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *thumbImageView;
@@ -18,7 +19,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *companyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *procUrlLabel;
 @property (weak, nonatomic) IBOutlet UILabel *procFuncLabel;
-
+//图集
+@property (weak, nonatomic) IBOutlet UIView *pictureView;
+@property (nonatomic,strong) NSArray *urlArray;
 @end
 
 @implementation ProjectSummaryTableViewController
@@ -38,6 +41,7 @@
                             @"projectId":self.pid
                             };
     [self.service GET:@"/project/getProjectDto" parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        self.dataDict = responseObject;
         self.thumbImageView.clipsToBounds = YES;
         NSString *url = [NSString stringWithFormat:@"%@/%@",UPLOAD_URL,responseObject[@"headPictUrl"]];
         [self.thumbImageView setImageWithURL:[NSURL URLWithString:url]];
@@ -49,8 +53,27 @@
         self.companyLabel.text = [StringUtil toString: responseObject[@"company"]];
         self.procUrlLabel.text = [StringUtil toString: responseObject[@"procUrl"]];
         self.procFuncLabel.text = [StringUtil toString: responseObject[@"procDetails"]];
+        //        图集
+        self.urlArray = [[StringUtil toString:self.dataDict[@"procShows"]] componentsSeparatedByString:@","];
+        LXGallery *gallery = [[LXGallery alloc] initWithFrame:CGRectMake(16, 40, SCREEN_WIDTH - 32, ceil(self.urlArray.count / 4.0) * IMAGE_WIDTH_WITH_PADDING)];
+        gallery.urlArray = self.urlArray;
+        gallery.vc = self;
+        [gallery reloadImagesList];
+        [self.pictureView addSubview:gallery];
+        [self.tableView reloadData];
     } noResult:^{
         
     }];
 }
+
+#pragma mark - tb delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 4) {
+        //        详情图片
+        CGFloat height = IMAGE_WIDTH_WITH_PADDING * ceil(self.urlArray.count / 4.0);
+        return 40 + height;
+    }
+    return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
+
 @end
