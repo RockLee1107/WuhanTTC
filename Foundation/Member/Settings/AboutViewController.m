3adffd05e7761dcc9973e1d9acc6c9701ca65417
iMac <huangxiujie@163.com>
2016-05-07 18:50:32 +0800

    关于我们

--- Foundation/Member/Settings/AboutViewController.m
+++ Foundation/Member/Settings/AboutViewController.m
@@ -7,12 +7,13 @@
 //
 
 #import "AboutViewController.h"
+#import "StandardViewController.h"
 
 @interface AboutViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
 @property (weak, nonatomic) IBOutlet UILabel *versionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
-@property (weak, nonatomic) IBOutlet UILabel *standardLabel;
+@property (weak, nonatomic) IBOutlet UIButton *standardButton;
 
 @end
 
@@ -21,16 +22,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setDynamicLayout];
     self.navigationItem.title = _naviTitle;
     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
-   
-//    self.logoImageView =[UIImage imageNamed:@"1.jpg"];
-    
     // app版本
     NSString *app_Version = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
     self.versionLabel.text = [NSString stringWithFormat:@"%@%@",self.versionLabel.text,app_Version];
     [self fetchData];
 }
+
+- (IBAction)standardButtonPress:(id)sender {
+    StandardViewController *vc = [[UIStoryboard storyboardWithName:@"Activity" bundle:nil] instantiateViewControllerWithIdentifier:@"standard"];
+    vc.type = @"2";
+    vc.naviTitle = @"用户协议";
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
 
 - (void)fetchData {
     NSDictionary *param = @{
@@ -42,4 +49,11 @@
     } noResult:nil];
 }
 
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 1 && indexPath.row == 1) {
+        return 60;
+    }
+    return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
 @end
